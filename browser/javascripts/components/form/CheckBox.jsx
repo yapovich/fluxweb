223774f5d6b940b84f6b6080cf323763ad1e507c
yebo <popoann16@163.com>
2016-04-27 14:36:14 +0800

    this is resource path changed

---
+++ browser/javascripts/components/form/CheckBox.jsx
@@ -0,0 +1,35 @@
+/**
+ * Created by yebo on 2016/4/11.
+ * 按钮
+ */
+var CheckBox = Flux.createView({
+    onChange:function(e){
+        var v=e.currentTarget.attributes["value"].value;
+        if(this.props.onSelected){
+            this.props.onSelected.call(null,v);
+        }
+    },
+    //按钮控件
+    getComponent:function(_props){
+        var html;
+        var group=_props.children;
+        if(group){
+            html=group.map(function(g){
+              var checked=g.checked?"checked":"";
+              var disabled=g.disabled?"disabled":"";
+              return <label className="checkbox" style={{display:"inline-block"}}>
+                  <input type="checkbox" checked={checked} disabled={disabled} onChange={this.onChange} value={g.value} className="custom-checkbox"/>
+                  <span className="icons">
+                      <span className="icon-unchecked"></span>
+                      <span className="icon-checked"></span>
+                  </span>{g.name}</label>;
+            }.bind(this));
+        }
+        return (<div className={_props.className}>{html}</div>);
+    },
+    render: function() {
+        var comp=this.getComponent(this.props);
+        return (comp);
+    }
+});
+module.exports = CheckBox;
